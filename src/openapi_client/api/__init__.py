de37dc134fff85643e80fba537cd30c0f1617995
Rehan <rehan@rehanrana.com>
2026-05-14 22:44:21 -0400

    update vendored sdk

--- src/openapi_client/api/__init__.py
+++ src/openapi_client/api/__init__.py
@@ -1,10 +1,6 @@
 # flake8: noqa
 
 # import apis into api package
-from openapi_client.api.enrichment_api import EnrichmentApi
 from openapi_client.api.extract_api import ExtractApi
-from openapi_client.api.fast_gpt_api import FastGPTApi
 from openapi_client.api.search_api import SearchApi
-from openapi_client.api.summarizer_api import SummarizerApi
-from openapi_client.api.translate_api import TranslateApi
 
